79ca80b3c72e14a3491782167eac5f0bba8608ba
Petr Hampl <2007hampl@seznam.cz>
2023-04-15 17:28:16 +0200

    Added draw gizmo on waypoints

--- Assets/Entity/Scripts/Ai/WaypointFinder.cs
+++ Assets/Entity/Scripts/Ai/WaypointFinder.cs
@@ -1,9 +1,26 @@
+using System;
 using UnityEngine;
 
 namespace Entity.Scripts.Ai
 {
+#if UNITY_EDITOR
+    [ExecuteAlways]
+#endif
+
     public class WaypointFinder : MonoBehaviour
     {
-        
+        private void Update()
+        {
+
+            
+        }
+
+        private void OnDrawGizmos()
+        {
+            
+#if UNITY_EDITOR
+            Gizmos.DrawSphere(transform.position,0.5f);
+#endif
+        }
     }
 }
